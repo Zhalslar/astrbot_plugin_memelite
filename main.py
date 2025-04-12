462530834e27a1b9faa4e4e476915f92e864420d
Zhalslar <zsl2936169201@gmail.com>
2025-04-12 20:21:40 +0800

    meme帮助的图片改成直接预先生成好

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+from pathlib import Path
 import aiohttp
 from meme_generator import Meme, get_memes
 from meme_generator.download import check_resources
@@ -21,43 +22,56 @@ from astrbot.core.star.filter.event_message_type import EventMessageType
 
 
 memes: list[Meme] = get_memes()
-meme_keywords_list = [keyword.lower() for meme in memes for keyword in meme.keywords] # 有序列表
-meme_keywords_set = set(meme_keywords_list) # 无序集合
+meme_keywords_list = [
+    keyword.lower() for meme in memes for keyword in meme.keywords
+]  # 有序列表
+meme_keywords_set = set(meme_keywords_list)  # 无序集合
 
-@register("astrbot_plugin_memelite", "Zhalslar", "表情包生成器，制作各种沙雕表情（本地部署，但轻量化）", "1.0.8")
+
+@register(
+    "astrbot_plugin_memelite",
+    "Zhalslar",
+    "表情包生成器，制作各种沙雕表情（本地部署，但轻量化）",
+    "1.0.8",
+    "https://github.com/Zhalslar/astrbot_plugin_memelite"
+)
 class MemePlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
         self.memes: list[Meme] = memes
         self.meme_keywords_list: list = meme_keywords_list
 
-        self.prefix: str = config.get('prefix', '')
-        self.fuzzy_match: int = config.get('fuzzy_match', True)
-        self.is_compress_image: bool = config.get('is_compress_image', True)
-        self.save_temp_image: bool = config.get('save_temp_image', False)
+        self.prefix: str = config.get("prefix", "")
+        self.fuzzy_match: int = config.get("fuzzy_match", True)
+        self.is_compress_image: bool = config.get("is_compress_image", True)
+        self.save_temp_image: bool = config.get("save_temp_image", False)
 
-        self.is_check_resources: bool = config.get('is_check_resources', True)
+        self.is_check_resources: bool = config.get("is_check_resources", True)
         if self.is_check_resources:
             logger.info("正在检查memes资源文件...")
             asyncio.create_task(check_resources())
 
+    @filter.command("meme帮助", alias={"表情帮助"})
+    async def meme_keywords_show(self, event: AstrMessageEvent):
+        "查看有哪些关键词可以触发meme"
+        image_path = Path(__file__).parent / "memes_help.jpg"
+        yield event.image_result(str(image_path))
 
-    @filter.command("表情列表", desc="查看有哪些关键词可以触发meme")
-    async def meme_keyword_show(self, event: AstrMessageEvent):
-        meme_keywords_str = "、".join(self.meme_keywords_list)
-        url = await self.text_to_image(meme_keywords_str, return_url=True)
-        yield event.image_result(url)
-
-    @filter.command("表情详情", desc="查看指定meme需要的参数")
-    async def meme_detail_show(self, event: AstrMessageEvent, keyword: str|None=None):
+    @filter.command("meme详情", alias={"表情详情"})
+    async def meme_details_show(
+        self, event: AstrMessageEvent, keyword: str | None = None
+    ):
+        "查看指定meme需要的参数"
         if not keyword:
             yield event.plain_result("未指定要查看的meme")
             return
-        target_keyword = next((k for k in meme_keywords_set if k in event.get_message_str()), None)
+        target_keyword = next(
+            (k for k in meme_keywords_set if k in event.get_message_str()), None
+        )
         if target_keyword is None:
             yield event.plain_result("未支持的meme关键词")
             return
-        meme: Meme|None = self.find_meme(target_keyword)
+        meme: Meme | None = self._find_meme(target_keyword)
         if not meme:
             yield event.plain_result("未找到相关meme")
             return
@@ -80,32 +94,35 @@ class MemePlugin(Star):
         ]
         yield event.chain_result(chain)
 
-
     @filter.event_message_type(EventMessageType.ALL)
     async def meme_handle(self, event: AstrMessageEvent):
         """
-         处理 meme 生成的核心逻辑。
+        处理 meme 生成的核心逻辑。
 
-         功能描述：
-         - 支持匹配所有 meme 关键词。
-         - 支持从原始消息中提取参数, 空格隔开参数。
-         - 支持引用消息传参 。
-         - 自动获取消息发送者、被 @ 的用户以及 bot 自身的相关参数。
-         """
+        功能描述：
+        - 支持匹配所有 meme 关键词。
+        - 支持从原始消息中提取参数, 空格隔开参数。
+        - 支持引用消息传参 。
+        - 自动获取消息发送者、被 @ 的用户以及 bot 自身的相关参数。
+        """
         message_str = event.get_message_str()
         if not message_str:  # 过滤非文本消息
             return
         message_list = message_str.split()
         first_arg = message_list[0]
 
-        if self.prefix: # 前缀模式
+        if self.prefix:  # 前缀模式
             if self.prefix not in first_arg:
                 return
 
         if self.fuzzy_match:
-            keyword = next((k for k in meme_keywords_set if k in message_str), None) # 模糊匹配模式
+            keyword = next(
+                (k for k in meme_keywords_set if k in message_str), None
+            )  # 模糊匹配模式
         else:
-            keyword = next((k for k in meme_keywords_set if k in message_list), None) # 精准匹配模式
+            keyword = next(
+                (k for k in meme_keywords_set if k in message_list), None
+            )  # 精准匹配模式
         if not keyword:
             return
 
@@ -113,7 +130,7 @@ class MemePlugin(Star):
         texts: List[str] = []
         args: dict[str, Any] = {}
 
-        meme: Meme|None = self.find_meme(keyword)
+        meme: Meme | None = self._find_meme(keyword)
         if meme is None:
             return
         max_images: int = meme.params_type.max_images
@@ -147,7 +164,6 @@ class MemePlugin(Star):
                     if text != keyword and text != self.prefix:
                         texts.append(text)
 
-
         # 如果有引用消息，也遍历之
         reply_seg = next((seg for seg in messages if isinstance(seg, Comp.Reply)), None)
         if reply_seg and reply_seg.chain:
@@ -162,13 +178,16 @@ class MemePlugin(Star):
 
         # aiocqhttp消息平台可调用Onebot接口“get_stranger_info”获取额外参数
         if event.get_platform_name() == "aiocqhttp":
-            from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import AiocqhttpMessageEvent
+            from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
+                AiocqhttpMessageEvent,
+            )
+
             assert isinstance(event, AiocqhttpMessageEvent)
             client = event.bot
-            target_id = target_ids[0] # 默认取用第一个用户
+            target_id = target_ids[0]  # 默认取用第一个用户
             user_info = await client.get_stranger_info(user_id=int(target_id))
-            target_name = user_info.get('nickname')
-            gender = user_info.get('sex')
+            target_name = user_info.get("nickname")
+            gender = user_info.get("sex")
             args["user_infos"] = [{"name": target_name, "gender": gender}]
 
         # 确保图片数量在min_images到max_images之间
@@ -184,15 +203,11 @@ class MemePlugin(Star):
         if len(texts) < max_texts and target_name:
             texts.append(target_name)
         if len(texts) < max_texts and default_texts:
-            texts.extend(default_texts[:max_texts - len(texts)])
+            texts.extend(default_texts[: max_texts - len(texts)])
         texts = texts[:max_texts]
 
         try:
-            image_io = await run_sync(meme)(
-                images=images,
-                texts=texts,
-                args=args
-            )
+            image_io = await run_sync(meme)(images=images, texts=texts, args=args)
 
         except MemeGeneratorException as e:
             logger.error(e.message)
@@ -204,17 +219,16 @@ class MemePlugin(Star):
                 return
         image_bytes = image_io.getvalue()
         chain = [Comp.Image.fromBytes(image_bytes)]
-        yield event.chain_result(chain) # type: ignore
+        yield event.chain_result(chain)  # type: ignore
 
-
-
-    def find_meme(self, meme_name: str) -> Meme | None:
+    def _find_meme(self, meme_name: str) -> Meme | None:
         """根据给定的meme名字查找并返回符合条件的第一个表情包对象"""
         search_name = meme_name.lower()
         for meme in self.memes:
             # 检查meme的关键字和别名是否包含目标名称
-            if (search_name == meme.key.lower() or
-                    any(keyword.lower() == search_name for keyword in meme.keywords)):
+            if search_name == meme.key.lower() or any(
+                keyword.lower() == search_name for keyword in meme.keywords
+            ):
                 return meme
         return None
 
@@ -236,7 +250,6 @@ class MemePlugin(Star):
         except Exception as e:
             raise ValueError(f"图片压缩失败: {e}")
 
-
     @staticmethod
     async def download_image(url: str) -> bytes | None:
         url = url.replace("https://", "http://")
@@ -249,26 +262,6 @@ class MemePlugin(Star):
             logger.error(f"图片下载失败: {e}")
 
     @staticmethod
-    async def get_events_on_history(month: str) -> str:
-        """
-        获取百度百科历史上的今天事件数据。
-
-        :param month: 月份，格式为两位数字，例如 "04" 表示四月
-        :return: 返回解析后的 JSON 数据字符串，如果失败则返回空字符串
-        """
-        try:
-            async with aiohttp.ClientSession() as client:
-                url = f"https://baike.baidu.com/cms/home/eventsOnHistory/{month}.json"
-                async with client.get(url) as response:
-                    if response.status != 200:
-                        logger.error(f"请求失败，状态码: {response.status}")
-                        return ""
-                    return await response.text(encoding="utf-8")
-        except Exception as e:
-            logger.error(f"任务处理失败: {e}")
-            return ""
-        
-    @staticmethod
     async def get_avatar(user_id: str) -> bytes:
         """下载头像"""
         avatar_url = f"https://q4.qlogo.cn/headimg_dl?dst_uin={user_id}&spec=640"
@@ -279,7 +272,4 @@ class MemePlugin(Star):
                 return await response.read()
         except Exception as e:
             logger.error(f"下载头像失败: {e}")
-            return b''
-
-
-
+            return b""
